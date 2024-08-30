c44454aa17d44244fdea75c391bb8352ea943629
Jayanthi <jayanthidhanabalan@gmail.com>
2024-08-30 19:25:38 +0530

    Maps implementation with Marker

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,41 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+//Instructions to every other class
+//on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable) {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+  //   addCompanyMarker(company: Company) {
+  //     new google.maps.Marker({ map: this.googleMap, position: company.location });
+  //   }
+}
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,9 +1,24 @@
-class User {
+import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
+
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "blue";
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    console.log(this.name);
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
 }
