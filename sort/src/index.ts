4cb075781f571761f799ea36147b57d14d937eb8
Jayanthi <jayanthidhanabalan@gmail.com>
2024-09-07 18:31:10 +0530

    Sort collection

---
+++ sort/src/index.ts
@@ -0,0 +1,23 @@
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+const numbersCollection = new NumbersCollection([10, 50, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xyaabg");
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(40);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
