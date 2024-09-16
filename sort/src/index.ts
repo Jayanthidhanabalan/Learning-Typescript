8cc41774410eb55ae860b7a9ed144b9898e4dd60
Jayanthi <jayanthidhanabalan@gmail.com>
2024-09-16 13:06:29 +0530

    Generic Collection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,13 +4,13 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 50, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+//const sorter = new Sorter(numbersCollection);
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xyaabg");
-const characterSorter = new Sorter(charactersCollection);
-characterSorter.sort();
+//const characterSorter = new Sorter(charactersCollection);
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +18,6 @@ linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(40);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+//const linkedListSorter = new Sorter(linkedList);
+linkedList.sort();
 linkedList.print();
